3d458460e061caa58794b85b6817301f4bc69f6f
Xavier VIRICEL <x.viricel@testamento.fr>
2025-08-29 12:54:38 +0200

    feat: add config gpg.sh

--- install.sh
+++ install.sh
@@ -15,6 +15,7 @@ source $XAVECORP_INSTALL/preflight/repositories.sh
 source $XAVECORP_INSTALL/preflight/first-run-mode.sh
 
 # Packaging
+echo "\n\n ==> Start packages config"
 source $XAVECORP_INSTALL/packages.sh
 source $XAVECORP_INSTALL/packaging/fonts.sh
 source $XAVECORP_INSTALL/packaging/zsh.sh
@@ -23,30 +24,32 @@ source $XAVECORP_INSTALL/packaging/applications.sh
 source $XAVECORP_INSTALL/packaging/webapps.sh
 
 # Configuration
+echo "\n\n ==> Start default config"
 source $XAVECORP_INSTALL/config/config.sh
 source $XAVECORP_INSTALL/config/theme.sh
 #source $XAVECORP_INSTALL/config/branding.sh
 #source $XAVECORP_INSTALL/config/git.sh
 source $XAVECORP_INSTALL/config/gpg.sh
-source $XAVECORP_INSTALL/config/timezones.sh
-source $XAVECORP_INSTALL/config/increase-sudo-tries.sh
-source $XAVECORP_INSTALL/config/increase-lockout-limit.sh
-source $XAVECORP_INSTALL/config/ssh-flakiness.sh
-source $XAVECORP_INSTALL/config/detect-keyboard-layout.sh
-source $XAVECORP_INSTALL/config/xcompose.sh
-source $XAVECORP_INSTALL/config/mise-ruby.sh
-source $XAVECORP_INSTALL/config/docker.sh
-source $XAVECORP_INSTALL/config/mimetypes.sh
-source $XAVECORP_INSTALL/config/localdb.sh
-source $XAVECORP_INSTALL/config/hardware/network.sh
-source $XAVECORP_INSTALL/config/hardware/fix-fkeys.sh
-source $XAVECORP_INSTALL/config/hardware/bluetooth.sh
-source $XAVECORP_INSTALL/config/hardware/printer.sh
-source $XAVECORP_INSTALL/config/hardware/usb-autosuspend.sh
-source $XAVECORP_INSTALL/config/hardware/ignore-power-button.sh
+#source $XAVECORP_INSTALL/config/timezones.sh
+#source $XAVECORP_INSTALL/config/increase-sudo-tries.sh
+#source $XAVECORP_INSTALL/config/increase-lockout-limit.sh
+#source $XAVECORP_INSTALL/config/ssh-flakiness.sh
+#source $XAVECORP_INSTALL/config/detect-keyboard-layout.sh
+#source $XAVECORP_INSTALL/config/xcompose.sh
+#source $XAVECORP_INSTALL/config/mise-ruby.sh
+#source $XAVECORP_INSTALL/config/docker.sh
+#source $XAVECORP_INSTALL/config/mimetypes.sh
+#source $XAVECORP_INSTALL/config/localdb.sh
+#source $XAVECORP_INSTALL/config/hardware/network.sh
+#source $XAVECORP_INSTALL/config/hardware/fix-fkeys.sh
+#source $XAVECORP_INSTALL/config/hardware/bluetooth.sh
+#source $XAVECORP_INSTALL/config/hardware/printer.sh
+#source $XAVECORP_INSTALL/config/hardware/usb-autosuspend.sh
+#source $XAVECORP_INSTALL/config/hardware/ignore-power-button.sh
 #source $XAVECORP_INSTALL/config/hardware/nvidia.sh
 
 # Login
+echo "\n\n ==> Start login config"
 source $XAVECORP_INSTALL/login/plymouth.sh
 source $XAVECORP_INSTALL/login/limine-snapper.sh
 source $XAVECORP_INSTALL/login/alt-bootloaders.sh
