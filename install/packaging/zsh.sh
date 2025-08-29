c0fe1baa9e4db84e4d9a984f4208d62434e7af53
Xavier VIRICEL <x.viricel@testamento.fr>
2025-08-29 12:15:06 +0200

    fix: add clean zsh config

--- install/packaging/zsh.sh
+++ install/packaging/zsh.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+if [ -d "$HOME/.oh-my-zsh" ] then
+    rm -rf "$HOME/.oh-my-zsh"
+fi
+
 # Install custom zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
