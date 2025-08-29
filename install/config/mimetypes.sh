3137096697e286d9323dfc89fce7cce23b2a68e8
Xavier VIRICEL <x.viricel@testamento.fr>
2025-08-29 18:58:35 +0200

    fix: resolve mime type

--- install/config/mimetypes.sh
+++ install/config/mimetypes.sh
@@ -17,23 +17,23 @@ xdg-mime default imv.desktop image/tiff
 xdg-mime default org.gnome.Evince.desktop application/pdf
 
 # Use Chromium as the default browser
-xdg-settings set default-web-browser chromium.desktop
-xdg-mime default chromium.desktop x-scheme-handler/http
-xdg-mime default chromium.desktop x-scheme-handler/https
+#xdg-settings set default-web-browser chromium.desktop
+#xdg-mime default chromium.desktop x-scheme-handler/http
+#xdg-mime default chromium.desktop x-scheme-handler/https
 
 # Open video files with mpv
-xdg-mime default mpv.desktop video/mp4
-xdg-mime default mpv.desktop video/x-msvideo
-xdg-mime default mpv.desktop video/x-matroska
-xdg-mime default mpv.desktop video/x-flv
-xdg-mime default mpv.desktop video/x-ms-wmv
-xdg-mime default mpv.desktop video/mpeg
-xdg-mime default mpv.desktop video/ogg
-xdg-mime default mpv.desktop video/webm
-xdg-mime default mpv.desktop video/quicktime
-xdg-mime default mpv.desktop video/3gpp
-xdg-mime default mpv.desktop video/3gpp2
-xdg-mime default mpv.desktop video/x-ms-asf
-xdg-mime default mpv.desktop video/x-ogm+ogg
-xdg-mime default mpv.desktop video/x-theora+ogg
-xdg-mime default mpv.desktop application/ogg
+#xdg-mime default mpv.desktop video/mp4
+#xdg-mime default mpv.desktop video/x-msvideo
+#xdg-mime default mpv.desktop video/x-matroska
+#xdg-mime default mpv.desktop video/x-flv
+#xdg-mime default mpv.desktop video/x-ms-wmv
+#xdg-mime default mpv.desktop video/mpeg
+#xdg-mime default mpv.desktop video/ogg
+#xdg-mime default mpv.desktop video/webm
+#xdg-mime default mpv.desktop video/quicktime
+#xdg-mime default mpv.desktop video/3gpp
+#xdg-mime default mpv.desktop video/3gpp2
+#xdg-mime default mpv.desktop video/x-ms-asf
+#xdg-mime default mpv.desktop video/x-ogm+ogg
+#xdg-mime default mpv.desktop video/x-theora+ogg
+#xdg-mime default mpv.desktop application/ogg
