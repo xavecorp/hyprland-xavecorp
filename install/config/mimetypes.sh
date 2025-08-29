d7bab3206650304a8373ba691b5b67c8336b0647
Xavier VIRICEL <x.viricel@testamento.fr>
2025-08-29 19:04:25 +0200

    fix: resolve mime type

--- install/config/mimetypes.sh
+++ install/config/mimetypes.sh
@@ -22,18 +22,18 @@ xdg-mime default org.gnome.Evince.desktop application/pdf
 #xdg-mime default chromium.desktop x-scheme-handler/https
 
 # Open video files with mpv
-#xdg-mime default mpv.desktop video/mp4
-#xdg-mime default mpv.desktop video/x-msvideo
-#xdg-mime default mpv.desktop video/x-matroska
-#xdg-mime default mpv.desktop video/x-flv
-#xdg-mime default mpv.desktop video/x-ms-wmv
-#xdg-mime default mpv.desktop video/mpeg
-#xdg-mime default mpv.desktop video/ogg
-#xdg-mime default mpv.desktop video/webm
-#xdg-mime default mpv.desktop video/quicktime
-#xdg-mime default mpv.desktop video/3gpp
-#xdg-mime default mpv.desktop video/3gpp2
-#xdg-mime default mpv.desktop video/x-ms-asf
-#xdg-mime default mpv.desktop video/x-ogm+ogg
-#xdg-mime default mpv.desktop video/x-theora+ogg
-#xdg-mime default mpv.desktop application/ogg
+xdg-mime default mpv.desktop video/mp4
+xdg-mime default mpv.desktop video/x-msvideo
+xdg-mime default mpv.desktop video/x-matroska
+xdg-mime default mpv.desktop video/x-flv
+xdg-mime default mpv.desktop video/x-ms-wmv
+xdg-mime default mpv.desktop video/mpeg
+xdg-mime default mpv.desktop video/ogg
+xdg-mime default mpv.desktop video/webm
+xdg-mime default mpv.desktop video/quicktime
+xdg-mime default mpv.desktop video/3gpp
+xdg-mime default mpv.desktop video/3gpp2
+xdg-mime default mpv.desktop video/x-ms-asf
+xdg-mime default mpv.desktop video/x-ogm+ogg
+xdg-mime default mpv.desktop video/x-theora+ogg
+xdg-mime default mpv.desktop application/ogg
