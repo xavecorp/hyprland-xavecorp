9335719b9212fe277bbf5b692cc69f6296f2ec6f
Xavier VIRICEL <x.viricel@testamento.fr>
2025-09-14 12:13:34 +0200

    fix: remove bad command

--- install/config/cursor/cursor.sh
+++ install/config/cursor/cursor.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-tar -xvf install/config/cursor/GoogleDot-Black.tar.gz -C /usr/share/icons
+XAVECORP_INSTALL=~/.local/share/xavecorp/install
+
+tar -xvf $XAVECORP_INSTALL/config/cursor/GoogleDot-Black.tar.gz -C /usr/share/icons
